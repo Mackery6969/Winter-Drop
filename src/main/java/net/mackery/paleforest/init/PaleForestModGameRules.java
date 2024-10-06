3664e23e3f72b67874310919297f0089d898e817
Mackery6969 <davishayden32@gmail.com>
2024-10-05 20:24:15 -0500

    Spreading is now default

--- src/main/java/net/mackery/paleforest/init/PaleForestModGameRules.java
+++ src/main/java/net/mackery/paleforest/init/PaleForestModGameRules.java
@@ -11,7 +11,7 @@ import net.minecraft.world.level.GameRules;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class PaleForestModGameRules {
 	public static final GameRules.Key<GameRules.BooleanValue> CREAKING_ENDERMAN = GameRules.register("creakingEnderman", GameRules.Category.MOBS, GameRules.BooleanValue.create(false));
-	public static final GameRules.Key<GameRules.BooleanValue> SPREADING_PALENESS = GameRules.register("spreadingPaleness", GameRules.Category.MISC, GameRules.BooleanValue.create(false));
+	public static final GameRules.Key<GameRules.BooleanValue> SPREADING_PALENESS = GameRules.register("spreadingPaleness", GameRules.Category.MISC, GameRules.BooleanValue.create(true));
 	public static final GameRules.Key<GameRules.BooleanValue> CREAKING_DARKNESS = GameRules.register("creakingDarkness", GameRules.Category.MOBS, GameRules.BooleanValue.create(false));
 	public static final GameRules.Key<GameRules.BooleanValue> CREAKING_KILL_ALL_MOBS_EXPERIMENT = GameRules.register("creakingKillAllMobsExperiment", GameRules.Category.MOBS, GameRules.BooleanValue.create(false));
 }
